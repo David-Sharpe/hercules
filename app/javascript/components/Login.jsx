2f84c44f1d502f0ffaeb8600f5ee4a95c21d18da
David-Sharpe <davidusbviolator@gmail.com>
2018-10-28 18:25:12 -0400

    Add Login component

---
+++ app/javascript/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Login extends React.Component {
+
+  render () {
+    console.log("login.jsx")
+    return (
+      <h2>Login</h2>
+    );
+  }
+}
+
+export default Login;
+
