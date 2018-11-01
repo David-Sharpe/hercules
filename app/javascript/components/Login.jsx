3c9c45bb09158512a157c0c096328f09ed02c9a3
David-Sharpe <5024886+David-Sharpe@users.noreply.github.com>
2018-10-31 20:27:01 -0400

    Add form components

--- app/javascript/components/Login.jsx
+++ app/javascript/components/Login.jsx
@@ -2,10 +2,27 @@ import React from 'react';
 
 class Login extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
   render () {
-    console.log("login.jsx")
     return (
-      <h2>Login</h2>
+      <form onSubmit={this.handleSubmit}>
+        <h2>Login</h2>
+        <label htmlFor="UserName">User Name:</label> <br />
+        <input id="UserName" type="text" name="UserName"/>
+        <br />
+        <label htmlFor="Password">Password:</label> <br />
+        <input id="Password" type="text" name="Password"/> <br />
+        <input type="submit" value="Login"/>
+      </form>
     );
   }
 }
