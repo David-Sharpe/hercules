b2394ad5384a413339f49ba9d45957091ec957c5
David Sharpe <5024886+David-Sharpe@users.noreply.github.com>
2019-05-16 20:03:37 -0400

    Added test for index.haml (#7)

--- spec/views/home/index.html.haml_spec.rb
+++ spec/views/home/index.html.haml_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "home/index.html.haml", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'renders the root page' do
+    render :template => "home/index.haml"
+
+    expect(rendered).to match /\//
+  end
 end
