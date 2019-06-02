7dd91156915e4476d1bc8f58cd2b689e41852476
David-Sharpe <5024886+David-Sharpe@users.noreply.github.com>
2019-06-02 17:11:01 -0400

    Add commas

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,9 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
-    ENV['AUTH0_CLIENT_ID']
-    ENV['AUTH0_CLIENT_SECRET']
-    ENV['AUTH0_DOMAIN']
+    ENV['AUTH0_CLIENT_ID'],
+    ENV['AUTH0_CLIENT_SECRET'],
+    ENV['AUTH0_DOMAIN'],
     callback_path: '/auth/oauth2/callback',
     authorize_params: {
       scope: 'openid profile'
